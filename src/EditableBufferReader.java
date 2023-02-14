3305b0ec55d05ac48562a523e4fec1b57dc91afc
DanielDiaz4401 <56456407+DanielDiaz4401@users.noreply.github.com>
2023-02-14 13:00:44 +0100

    Added readLine() method

---
+++ src/EditableBufferReader.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+class EditableBufferedReader extends BufferedReader{
+
+	private InputStreamReader in;
+	private char[] buffer;
+	private final int DEFAULT_LENGTH=256;
+	private int cursor;
+
+	public EditableBufferedReader(InputStreamReader in){
+		super(in);
+	}
+
+	/*
+	 * Passa la consola de mode cooked a mode raw.
+	 * */
+	public void setRaw(){
+	}
+
+	/*
+	 * Passa la consola de raw cooked a mode cooked.
+	 * */
+	public void unsetRaw(){
+	}
+
+	/*
+	 * Llegeix el següent caràcter o la següent tecla de cursor.
+	 * */
+	public int read(){
+		return 0;
+	}
+
+	/*
+	 * Llegeix la línia amb possibilitat d’editar-la.
+	 * */
+	public String readLine() throws IOException{
+		String buffer=new String();
+		char newChar;
+		do{
+			newChar = (char) super.read();
+			buffer+=newChar;
+		}while(newChar!='\n');
+		return buffer;
+	}
+}
