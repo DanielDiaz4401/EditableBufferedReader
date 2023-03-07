f2148a803f050c67ac6e5e4ab6b4d0dbe6c5d992
DanielDiaz4401 <danieldiaz4401@gmail.com>
2023-03-07 11:56:22 +0100

    Changed imports

--- src/EditableBufferReader.java
+++ src/EditableBufferReader.java
@@ -1,45 +1,47 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class EditableBufferedReader extends BufferedReader{
+class EditableBufferedReader extends BufferedReader {
 
 	private InputStreamReader in;
 	private char[] buffer;
-	private final int DEFAULT_LENGTH=256;
+	private final int DEFAULT_LENGTH = 256;
 	private int cursor;
 
-	public EditableBufferedReader(InputStreamReader in){
+	public EditableBufferedReader(InputStreamReader in) {
 		super(in);
 	}
 
 	/*
 	 * Passa la consola de mode cooked a mode raw.
-	 * */
-	public void setRaw(){
+	 */
+	public void setRaw() {
 	}
 
 	/*
 	 * Passa la consola de raw cooked a mode cooked.
-	 * */
-	public void unsetRaw(){
+	 */
+	public void unsetRaw() {
 	}
 
 	/*
 	 * Llegeix el següent caràcter o la següent tecla de cursor.
-	 * */
-	public int read(){
+	 */
+	public int read() {
 		return 0;
 	}
 
 	/*
 	 * Llegeix la línia amb possibilitat d’editar-la.
-	 * */
-	public String readLine() throws IOException{
-		String buffer=new String();
+	 */
+	public String readLine() throws IOException {
+		String buffer = new String();
 		char newChar;
-		do{
+		do {
 			newChar = (char) super.read();
-			buffer+=newChar;
-		}while(newChar!='\n');
+			buffer += newChar;
+		} while (newChar != '\n');
 		return buffer;
 	}
 }
